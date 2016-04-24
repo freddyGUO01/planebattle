2e8c8358484b4c69855632cb3e40e185c019e134
QiCheng <lixin65535@126.com>
2016-04-25 02:09:23 +0800

    #

--- assets/main.js
+++ assets/main.js
@@ -46,6 +46,8 @@ cc.Class({
         score: 0 , 
         e_count: 0,   // 打败的敌机数
         heroBulletPower: 10 ,  // 英雄子弹对于boss的威力
+        heroBoundLeft: 0,   //  英雄对于屏幕左右边界(canvas本地坐标系)(boss和enemy的宽度和hero相同，所以也是它们的边界)
+        heroBoundRight: 0,
     },
     
     onPlayerClick: function(event) {
@@ -53,7 +55,16 @@ cc.Class({
         if(!this.heroIsLife)    return ;
         
         var loc = event.getLocation();
-        //cc.log('Hello! x '+ loc.x +' y '+ loc.y);
+        
+        /*
+        if(loc.x < this.hero.width * this.hero.scaleX / 2){
+            loc.x = this.hero.width * this.hero.scaleX / 2 ;  // 防止超出边界
+        }
+        
+        if(loc.x > this.size.width - this.hero.width * this.hero.scaleX / 2 ){
+            loc.x = this.size.width - this.hero.width * this.hero.scaleX / 2;
+        }
+        */
         
         var hero_loc = this.hero.getPosition();
         
@@ -63,7 +74,15 @@ cc.Class({
         _speed = Math.abs(_speed) / 300;
         
         var _conv1 = this.node.convertToNodeSpaceAR(cc.v2(loc.x, loc.y));       //  设置几点位置需要转换到本地坐标系
-        //cc.log('convert! x '+ _conv1.x +' y '+ _conv1.y);
+        
+        
+        
+        if(_conv1.x < this.heroBoundLeft){
+            _conv1.x = this.heroBoundLeft ;
+        }
+        if(_conv1.x > this.heroBoundRight){
+            _conv1.x = this.heroBoundRight ;
+        }
         
         var hero_go = cc.moveTo(_speed, cc.p(_conv1.x, hero_loc.y))
         this.hero.stopAllActions();
@@ -82,6 +101,13 @@ cc.Class({
         this.node.addChild(_enemy);
         var _conv = this.node.convertToNodeSpaceAR(cc.v2(num, 0));
         
+        if(_conv.x < this.heroBoundLeft){
+            _conv.x = this.heroBoundLeft ;
+        }
+        if(_conv.x > this.heroBoundRight){
+            _conv.x = this.heroBoundRight ;
+        }
+        
         _enemy.setPosition(_conv.x, this.size.height/2);
         
         var _go = cc.moveTo(this.speed, cc.p(_conv.x, -(this.size.height/2+(this.enemy.height*this.enemy.scaleY/2)))) ;
@@ -89,11 +115,11 @@ cc.Class({
         var self = this ;
         _enemy.runAction(cc.sequence(_go, cc.callFunc(function(){ cc.js.array.remove(self.enemyArr, _enemy); }), cc.removeSelf()));
         
-        //cc.log('canvas child count : ' +this.node.childrenCount);
+        
     } ,
     
     onCreateBullet: function() {
-        //cc.log('on create bullet is called');
+        
         if(!this.heroIsLife)    return ;
         
         var cpos = this.hero.getPosition();
@@ -116,12 +142,12 @@ cc.Class({
     } ,
     
     removeBulletFromArr: function(obj) {
-        //cc.log('removeBulletFromArr is called');
+        
         cc.js.array.remove(this.bulletArr, obj);
     } ,
     
     onCreateBossBullet: function() {
-        if(!this.heroIsLife ||　this.bossArr.lenght <= 0)    return ;
+        if(!this.heroIsLife ||　this.bossArr.length <= 0)    return ;
         
         
         for(var _i in this.bossArr){
@@ -138,13 +164,20 @@ cc.Class({
             
             var _go = cc.moveTo(this.speed, cc.p(cpos.x, (this.size.height/2+(this.bullet.height*this.bullet.scaleY/2)) * -1 )) ;
             
-            var self = this ;
-            _bullet.runAction(cc.sequence(_go, cc.callFunc(function(){ cc.js.array.remove(self.bossBulletArr, _bullet); }), cc.removeSelf()));    
+            //var self = this ;
+            _bullet.runAction(cc.sequence(_go, cc.callFunc(this.removeBossBulletFromArr.bind(this), _bullet), cc.removeSelf()));    
             
         }
         
         
     },
+    
+    
+    removeBossBulletFromArr: function(obj) {
+        
+        cc.js.array.remove(this.bossBulletArr, obj);
+        
+    } ,
     
     createBoss: function(level){
         if(!this.heroIsLife)    return ;
@@ -159,7 +192,9 @@ cc.Class({
         //var num = this.size.width /2 ;
         //var _conv = this.node.convertToNodeSpaceAR(cc.v2(num, 0));
         
-        var _boss_y = this.size.height/2 - this.enemy.height*this.enemy.scaleY ;
+        var margin_top = this.bossArr.length * ( this.boss.height*this.boss.scaleY+20 ) ;
+        
+        var _boss_y = this.size.height/2 - margin_top - this.boss.height*this.boss.scaleY ;
         
         //_boss.setPosition(_conv.x, _boss_y);
         _boss.setPosition(boss_xf*-1, _boss_y);
@@ -168,7 +203,7 @@ cc.Class({
         /*
         var bar = _boss.getComponent(cc.ProgressBar)
         bar.progress = 0.5;
-        cc.log(bar)
+        
         */
         
         this.bossArr.push({_node:_boss, _hp:100})   // 可以如此封装一个需要多个参数的npc
@@ -186,10 +221,10 @@ cc.Class({
         var scene = cc.director.getScene();
         this.size = cc.director.getVisibleSize();
         
-        cc.log(this.size)
+        this.heroBoundLeft  = (this.size.width / 2 - this.hero.width * this.hero.scaleX / 2 ) * -1 ;
+        this.heroBoundRight = this.size.width / 2 - this.hero.width * this.hero.scaleX / 2 ;
         
-        //cc.log(this.node.width+' '+this.node.height);
-        //cc.log(this.enemy.width+' '+this.enemy.height);
+        
         
         var self = this;
         
@@ -202,7 +237,7 @@ cc.Class({
         //console.log(this.score_label)
         var score_position  = this.score_label.getPosition();
         this.score_label.setPosition(this.size.width/2*-1+50,score_position.y);
-        cc.log(this.score_label.getPosition())
+        
         this.score_label.setLocalZOrder(4)
         
         this.node.on(cc.Node.EventType.TOUCH_START, function ( event ) {
@@ -271,7 +306,7 @@ cc.Class({
                     cc.log(e.message);
                 }
             }
-            //cc.log(this.bulletArr.length)
+            
             for(var _i in this.bulletArr){
                 if(!this.bulletArr[_i].isChildOf(this.node)){
                     //如果已经被移除当前画布，则从数组移除成员
@@ -279,10 +314,10 @@ cc.Class({
                 }
                 
                 try{
-                    //cc.log(this.bulletArr[_i])
+                    //检测敌机死亡
                     var rect = this.bulletArr[_i].getBoundingBoxToWorld() ;
                     if( rect.intersects ( this.enemyArr[_j].getBoundingBoxToWorld() ) ){
-                        cc.log('is intersects');
+                        
                         var aa = this.bulletArr[_i] ;
                         var bb = this.enemyArr[_j] ;
                         
@@ -311,7 +346,9 @@ cc.Class({
                         this.score += this.enemy_score;
                         if( (++this.e_count) % 10 == 0 )
                         {
-                            this.createBoss();   // 每打死10个小怪，奖励一个boss战
+                            
+                            //if(this.bossArr.length<=0)
+                                this.createBoss();   // 每打死10个小怪，奖励一个boss战
                         }
                         
                         EnemyExistFlag = false; //设置敌机为不存在
@@ -336,7 +373,7 @@ cc.Class({
         
         //boss与hero的子弹碰撞检测
         if(this.bossArr.length>0){
-            cc.log('boss break test ');
+            
             for(var _i in this.bossArr) {
                 
                 var _boss = this.bossArr[_i] ;
@@ -344,43 +381,37 @@ cc.Class({
                 
                 for(var _j in this.bulletArr) {
                     try{
-                    if(_bossBox.intersects(this.bulletArr[_j].getBoundingBoxToWorld())) {
-                        var aa = this.bulletArr[_j] ;
-                        var bb = _boss._node ;
-                        
-                        _boss._hp -= this.heroBulletPower ; 
-                        if (_boss._hp <=0 ) _boss._hp = 0 ;
-                        
-                        var bar = _boss._node.getComponent(cc.ProgressBar) ;
-                        bar.progress = _boss._hp / 100 ;
-                        //cc.log(bar)
-                        
-                        
-                        var _boom = cc.instantiate(this.boom) ;
-                        this.node.addChild(_boom);
-                        _boom.setLocalZOrder(3);
-                        _boom.setPosition(bb.getPosition());
-                        
-                         //播放爆炸动画后销毁
-                        aa.removeFromParent();
-                        //bb.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf())) ;
-                        _boom.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf()));
-                        
-                        //先把他们移除，防止二次检测
-                        //cc.js.array.removeAt(this.enemyArr, _j);
-                        cc.js.array.removeAt(this.bulletArr, _j);
-                        
-                        
-                        if (_boss._hp <=0 ){
-                            bb.runAction(cc.sequence(cc.delayTime(0.1), cc.removeSelf())) ;
-                            cc.js.array.removeAt(this.bossArr, _i);
-                            this.score += this.boss_score;
+                        if(_bossBox.intersects(this.bulletArr[_j].getBoundingBoxToWorld())) {
+                            var aa = this.bulletArr[_j] ;
+                            var bb = _boss._node ;
+                            
+                            _boss._hp -= this.heroBulletPower ; 
+                            if (_boss._hp <=0 ) _boss._hp = 0 ;
+                            
+                            var bar = _boss._node.getComponent(cc.ProgressBar) ;
+                            bar.progress = _boss._hp / 100 ;
+                            
+                            var _boom = cc.instantiate(this.boom) ;
+                            this.node.addChild(_boom);
+                            _boom.setLocalZOrder(3);
+                            _boom.setPosition(bb.getPosition());
+                            
+                            //播放爆炸动画后销毁
+                            aa.removeFromParent();
+                            _boom.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf()));
+                            
+                            //先把他们移除，防止二次检测
+                            cc.js.array.removeAt(this.bulletArr, _j);
+                            
+                            
+                            if (_boss._hp <=0 ){
+                                cc.js.array.remove(this.bossArr, _boss);
+                                bb.runAction(cc.sequence(cc.delayTime(0.1), cc.removeSelf())) ;
+                                this.score += this.boss_score;
+                            }
                         }
-                    }
                     }catch(e){
-                        
                         cc.log(e.message);
-                        cc.log(this.bulletArr[_j])
                     }
                 }
                 
@@ -394,33 +425,28 @@ cc.Class({
         {
             var _heroBox = this.hero.getBoundingBoxToWorld();
             for(var _i in this.bossBulletArr){
-                if(_heroBox.intersects(this.bossBulletArr[_i].getBoundingBoxToWorld())) {
-                    this.heroIsLife = false ;
-                    this.hero.stopAllActions();   //  防止爆炸效果出现位移
-                    
-                    var bb = this.bossBulletArr[_i] ;
-                    bb.removeFromParent();
-                    //bb.stopAllActions();
-                    
-                    var _boom = cc.instantiate(this.boom) ;
-                    this.node.addChild(_boom);
-                    _boom.setPosition( this.hero.getPosition() );
-                    _boom.setLocalZOrder(3);
-                    
-                    /*
-                    var _boom2 = cc.instantiate(this.boom) ;
-                    this.node.addChild(_boom2);
-                    _boom2.setPosition( bb.getPosition() );
-                    _boom2.setLocalZOrder(3);
-                    */
-                    
-                    //bb.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf())) ;
-                    this.hero.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf())) ;
-                    _boom.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf()));
-                    //_boom2.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf()));
-                    
-                    this.unscheduleAllCallbacks ( );  // 停止所有创建物体
-                    break ; // 游戏结束
+                try{
+                    if(_heroBox.intersects(this.bossBulletArr[_i].getBoundingBoxToWorld())) {
+                        this.heroIsLife = false ;
+                        this.hero.stopAllActions();   //  防止爆炸效果出现位移
+                        
+                        var bb = this.bossBulletArr[_i] ;
+                        cc.js.array.remove(this.bossBulletArr, bb);
+                        bb.removeFromParent();
+                        
+                        var _boom = cc.instantiate(this.boom) ;
+                        this.node.addChild(_boom);
+                        _boom.setPosition( this.hero.getPosition() );
+                        _boom.setLocalZOrder(3);
+                        
+                        this.hero.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf())) ;
+                        _boom.runAction(cc.sequence(cc.delayTime(0.5), cc.removeSelf()));
+                        
+                        this.unscheduleAllCallbacks();  // 停止所有创建物体
+                        break ; // 游戏结束
+                    }
+                }catch(e){
+                    cc.log(e.message);
                 }
             }
         }
--- assets/start.js
+++ assets/start.js
@@ -14,12 +14,17 @@ cc.Class({
         btn:{
             default:null,
             type:cc.Button
+        } ,
+        loading:{
+            default:null,
+            type:cc.Node
         }
     },
 
     // use this for initialization
     onLoad: function () {
-
+        
+        this.loading.getComponent(cc.Label).enabled = false ;
     },
 
     // called every frame, uncomment this function to activate update callback
@@ -28,6 +33,22 @@ cc.Class({
     // },
     
     onStartClick:function () {
-        cc.director.loadScene('battle')
+        this.btn.interactable = false;
+        this.loading.getComponent(cc.Label).enabled = true ;
+        cc.log('load battle scene') ;
+        
+        var self = this;
+        
+        this.schedule(function(){
+            var str = self.loading.getComponent(cc.Label).string;
+            if(str == 'loading.') str = 'loading..' ;
+            else if(str == 'loading..') str = 'loading...';
+            else str = 'loading.';
+            
+            
+            self.loading.getComponent(cc.Label).string = str;
+        }, 1);
+        
+        cc.director.loadScene('battle');
     }
 });
